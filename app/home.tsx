d07688393f81967673e65b49e6ce78a244f54f6e
Rakib Ahmed <RAhmed@hvac.mea.com>
2022-11-29 15:58:52 -0500

    Content on Home

--- app/home.tsx
+++ app/home.tsx
@@ -12,6 +12,7 @@ export const capitalizeAllWords = (string: any) => {
 
 export default function Home() {
   const { state, setState } = useContext(StateContext);
+  const custom = (e: any) => console.log(e);
 
   useEffect(() => {
     setState({ 
@@ -22,12 +23,22 @@ export default function Home() {
     console.log(`Home`, state);
   }, [])
 
-  return <div className={`inner`}>
-      <h1>Home</h1>
-      <div className="column rightColumn">
-        {state.devEnv && <h2>Clicks: {state.updates}</h2>}
-        <h2>Env: {state.devEnv ? `Dev` : `Prod`}</h2>
-        {state.devEnv && <h2>State: {state.page == `` ? `Home` : capitalizeAllWords(state.page)}</h2>}
-      </div>
+  return <div className={`inner pageInner`}>
+    <section className={`topContent`}>
+        <h1>Home</h1>
+      <div className={`column rightColumn`}>
+          {state.devEnv && <h2>Clicks: {state.updates}</h2>}
+          <h2>Env: {state.devEnv ? `Dev` : `Prod`}</h2>
+          {state.devEnv && <h2>State: {state.page == `` ? `Home` : capitalizeAllWords(state.page)}</h2>}
+        </div>
+      </section>
+      <section>
+        <article>
+          <h2><i>Content</i></h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, illo? Pariatur facilis iusto aspernatur, magnam praesentium quisquam eum, laboriosam nihil odit, fugiat quae ipsam itaque sequi numquam! Tempore, officiis rerum.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, illo? Pariatur facilis iusto aspernatur, magnam praesentium quisquam eum, laboriosam nihil odit, fugiat quae ipsam itaque sequi numquam! Tempore, officiis rerum.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, illo? Pariatur facilis iusto aspernatur, magnam praesentium quisquam eum, laboriosam nihil odit, fugiat quae ipsam itaque sequi numquam! Tempore, officiis rerum.</p>
+        </article>
+      </section>
   </div>
 }
