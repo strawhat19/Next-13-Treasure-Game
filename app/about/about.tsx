5cb569a7e45ec24d0b939419147d5f1c95725646
Rakib Ahmed <rakib987@gmail.com>
2022-11-30 19:34:33 -0500

    State of Pages

--- app/about/about.tsx
+++ app/about/about.tsx
@@ -3,14 +3,15 @@ import { useContext, useEffect } from 'react';
 import { capitalizeAllWords, StateContext } from '../home';
 
 export default function About() {
-    const { state, setState } = useContext(StateContext);
+    const { state, setState, page, setPage } = useContext(StateContext);
 
     useEffect(() => {
       setState({ 
         ...state,
         updates: state.updates+1, 
-        page: window.location.pathname.replace(`/`,``),
+        // page: window.location.pathname.replace(`/`,``),
       });
+      setPage(window.location.pathname.replace(`/`,``));
       console.log(`About`, state);
     }, [])
 
@@ -18,6 +19,7 @@ export default function About() {
         <h1>About</h1>
         <div className="column rightColumn">
             <h2>Clicks: {state.updates}</h2>
+            <h2>State: {page}</h2>
             <h2>State: {state.page == `` ? `Home` : capitalizeAllWords(state.page)}</h2>
         </div>
     </div>
