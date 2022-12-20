631343b5857c6835c45647e6ea4419003f86de3c
Rakib Ahmed <RAhmed@hvac.mea.com>
2022-12-20 14:23:01 -0500

    Store in Google Sheets DB

--- app/about/about.tsx
+++ app/about/about.tsx
@@ -1,50 +1,11 @@
 'use client';
 import { StateContext } from '../home';
 import Banner from '../components/banner';
-import Table from '../components/table';
 import AuthForm from '../components/form';
 import { useContext, useEffect } from 'react';
 
 export default function About() {
   const { updates, setUpdates, user, setPage } = useContext(StateContext);
-
-  const colData = [
-    {
-      name: 'John Doe',
-      age: 32,
-      address: '123 Main St.',
-      cell: `data`
-    },
-    {
-      name: 'Jane Smith',
-      age: 26,
-      address: '456 Park Ave.',
-      cell: `data`
-    },
-    // more data...
-  ];
-
-  const nums = [1,2]
-
-  const data = [
-    colData,
-    nums
-  ];
-
-  const columns = [
-    {
-      Header: 'Name',
-      accessor: 'name', // accessor is the "key" in the data
-    },
-    {
-      Header: 'Age',
-      accessor: 'age',
-    },
-    {
-      Header: 'Address',
-      accessor: 'address',
-    },
-  ];
 
     useEffect(() => {
       setPage(`About`);
@@ -60,14 +21,6 @@ export default function About() {
             <div className="flex auth">
               <AuthForm />
             </div>
-          </article>
-        </div>
-      </section>
-      <section>
-        <div className="inner">
-          Tsble
-          <article>
-            <Table columns={columns.map(col => col.Header)} data={[colData.map(col => col.address)]} />
           </article>
         </div>
       </section>
