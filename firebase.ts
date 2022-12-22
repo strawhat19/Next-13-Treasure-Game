572f2129e90f8854b831dbf06cd0314cca755a84
Rakib Ahmed <rakib987@gmail.com>
2022-12-21 20:03:40 -0500

    PROCESS ENV

--- firebase.ts
+++ firebase.ts
@@ -1,20 +1,17 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-// import auth, { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC7knJwt7QdVhNsbEPCM58MVZh2EZHLsqI",
-  authDomain: "rahmednoodles.firebaseapp.com",
+  appId: process.env.APPID,
+  apiKey: process.env.APIKEY,
   projectId: "rahmednoodles",
-  storageBucket: "rahmednoodles.appspot.com",
-  messagingSenderId: "753102282469",
-  appId: "1:753102282469:web:efdfc59f94cc74effa3e0c",
-  measurementId: "G-HWKFLC5TQX"
+  authDomain: process.env.AUTHDOMAIN,
+  storageBucket: process.env.STORAGEBUCKET,
+  measurementId: process.env.MEASUREMENTID,
+  messagingSenderId: process.env.MESSAGINGSENDERID,
 };
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
-// export const autho = getAuth(initializeApp(firebaseConfig));
-// export const uauth = auth;
 
 export default app;
